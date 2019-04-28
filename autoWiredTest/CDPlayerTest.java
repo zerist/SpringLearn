8be8c4e4df8073ec831acfeed97680165bce93c7
zerist <15871792739@163.com>
2019-04-28 21:29:15 +0800

    junit

--- autoWiredTest/CDPlayerTest.java
+++ autoWiredTest/CDPlayerTest.java
@@ -3,12 +3,19 @@ package autoWiredTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = CDPlayerConfig.class)
 public class CDPlayerTest {
     @Autowired
     private CompactDisc cd;
 
-
+    @Test
+    public void cdShouldNotBeNull(){
+        assertNotNull(cd);
+    }
 
 }
